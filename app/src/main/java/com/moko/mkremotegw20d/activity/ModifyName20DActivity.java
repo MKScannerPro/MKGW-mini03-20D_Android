23e0e464249a7b69a27309f437e046fab495ff50
liuwenzheng <329541594@qq.com>
2026-09-21 14:59:33 +0800

    解决安卓最新版本返回无效问题

--- app/src/main/java/com/moko/mkremotegw20d/activity/ModifyName20DActivity.java
+++ app/src/main/java/com/moko/mkremotegw20d/activity/ModifyName20DActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.text.InputFilter;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -64,11 +63,7 @@ public class ModifyName20DActivity extends BaseActivity<ActivityModifyDeviceName
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            return false;
-        }
-        return super.onKeyDown(keyCode, event);
+    public void onBackPressed() {
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
